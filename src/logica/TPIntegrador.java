4c72f2b86141ed90b814b3a153ec3a5b384e42f5
lauta <lauta@Lautaro>
2026-02-12 20:55:00 +0100

    Proyecto SuperMercado

---
+++ src/logica/TPIntegrador.java
@@ -0,0 +1,16 @@
+
+package logica;
+
+import igu.Principal;
+
+
+public class TPIntegrador {
+
+  
+    public static void main(String[] args) {
+       Principal ventana = new Principal();
+       ventana.setVisible(true); 
+       ventana.setLocationRelativeTo(null);
+    }
+    
+}
